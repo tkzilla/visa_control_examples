4745760d8b7f836eff0493d71ad7ca45c64f85b1
Morgan <morgan.allison@tektronix.com>
2017-11-30 14:20:50 -0500

    Updated docstrings for socket_instrument.py and brought into conformity with PEP8.

--- Python/socket_instrument.py
+++ Python/socket_instrument.py
@@ -2,14 +2,14 @@
 Socket Instrument Class
 Author: Morgan Allison
 Based on awg7k_sock.py, written by Carl Murdock
-Date edited: 10/17
+Updated: 11/17
 This program provides a socket interface to Tektronix test equipment.
 It handles sending commands, receiving query results, and
 reading binary block data.
 Windows 7 64-bit, TekVISA 4.0.4
-Python 3.6.1 64-bit (Anaconda 4.4.0)
-NumPy 1.13.1
-Tested on RSA5126B, RSA7100A, DPO77002SX, RSA507A+SignalVu-PC, MSO58
+Python 3.6.2 64-bit
+NumPy 1.13.3
+Tested on RSA5126B, RSA7100A, DPO77002SX, RSA507A + SignalVu-PC, MSO58
 """
 
 import socket
@@ -20,9 +20,10 @@ class BinblockError(Exception):
     """Binary Block Exception class"""
     pass
 
+
 class SocketInstrument:
     def __init__(self, host, port, timeout=10):
-        """sets up instrument socket"""
+        """Open socket connection with settings for instrument control."""
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.setblocking(False)
         self.socket.settimeout(timeout)
@@ -31,80 +32,93 @@ class SocketInstrument:
         self.instId = self.query('*idn?')
 
     def disconnect(self):
-        """closes connection"""
+        """Gracefully close connection."""
         self.socket.shutdown(socket.SHUT_RDWR)
         self.socket.close()
 
     def query(self, cmd):
-        """Sends query to instrument and returns reply as string"""
-        # send message
+        """Sends query to instrument and returns reply as string."""
         self.write(cmd)
-        
-        # continuously read until termination character is found
+
+        # Read continuously until termination character is found.
         response = b''
         while response[-1:] != b'\n':
             response += self.socket.recv(1024)
-        
-        # strip out whitespace characters
+
+        # Strip out whitespace and return.
         return response.decode().strip()
 
     def write(self, cmd):
-        """writes a command string to instrument"""
+        """Write a command string to instrument."""
         msg = '{}\n'.format(cmd)
         self.socket.send(msg.encode('latin_1'))
 
     def binblockread(self, dtype=np.int8, debug=False):
-        """Reads a binary block with format '#<headerLength><numBytes>DATA'
-        Returns a NumPy array of the specified data type"""
-        
-        # Read # character, throw exception if not present
+        """Read data with IEEE 488.2 binary block format
+
+        The waveform is formatted as:
+        #<x><yyy><data><newline>, where:
+        <x> is the number of y bytes. For example, if <yyy>=500, then <x>=3.
+        NOTE: <x> is a hexadecimal number.
+        <yyy> is the number of bytes to transfer. Care must be taken
+        when selecting the data type used to interpret the data.
+        The dtype argument used to read the data must match the data
+        type used by the instrument that sends the data.
+        <data> is the curve data in binary format.
+        <newline> is a single byte new line character at the end of the data.
+        """
+
+        # Read # character, raise exception if not present.
         if self.socket.recv(1) != b'#':
             raise BinblockError('Data in buffer is not in binblock format.')
-        
-        # Extract header length and number of bytes in binblock
+
+        # Extract header length and number of bytes in binblock.
         headerLength = int(self.socket.recv(1).decode('latin_1'), 16)
         numBytes = int(self.socket.recv(headerLength).decode('latin_1'))
-        
+
         if debug:
             print('Header: #{}{}'.format(headerLength, numBytes))
-        
+
         rawData = bytearray(numBytes)
         buffer = memoryview(rawData)
-        
-        # While there is data left to read
+
+        # While there is data left to read...
         while numBytes:
-            # Read data from instrument into buffer
+            # Read data from instrument into buffer.
             bytesRecv = self.socket.recv_into(buffer, numBytes)
-            # Slice buffer to preserve data already written to it
+            # Slice buffer to preserve data already written to it.
             buffer = buffer[bytesRecv:]
-            # Subtract bytes read from total bytes
+            # Subtract bytes received from total bytes.
             numBytes -= bytesRecv
             if debug:
-                print('numBytes: {}, bytesRecv: {}'.format(numBytes, bytesRecv))
+                print('numBytes: {}, bytesRecv: {}'.format(
+                    numBytes, bytesRecv))
 
-        # Receive termination character
+        # Receive termination character.
         term = self.socket.recv(1)
         if debug:
             print('Term char: ', term)
-        # If term char is incorrect or not present, raise exception
+        # If term char is incorrect or not present, raise exception.
         if term != b'\n':
-            print('Term char: {}, rawData Length: {}'.format(term, len(rawData)))
+            print('Term char: {}, rawData Length: {}'.format(
+                term, len(rawData)))
             raise BinblockError('Data not terminated correctly.')
-        
-        # Convert binary data to NumPy array and return
+
+        # Convert binary data to NumPy array of specified data type and return.
         return np.frombuffer(rawData, dtype=dtype)
 
     def get_error(self):
+        """Query the error status register and read all events."""
         self.query('*esr?')
         # return self.query('system:error:all?')
         return self.query('allev?')
 
 
 def rsa_example(ipAddress):
-    """This is for testing pretty much any RSA"""
+    """Test generic RSA connection, signal capture, and data transfer."""
     inst = SocketInstrument(host=ipAddress, port=4000, timeout=3)
     print(inst.instId)
-    
+
     inst.write('system:preset')
     inst.write('initiate:continuous off')
     inst.write('sense:spectrum:points:count P64001')
@@ -113,17 +127,17 @@ def rsa_example(ipAddress):
         inst.query('*opc?')
         inst.write('fetch:spectrum:trace?')
         data = inst.binblockread(dtype=np.float32, debug=True)
-    
+
     print(inst.get_error())
     inst.disconnect()
     return data
 
 
 def scope_example(ipAddress):
-    """This is for testing pretty much any scope"""
+    """Test generic scope connection, signal capture, and data transfer."""
     inst = SocketInstrument(host=ipAddress, port=4000, timeout=3)
     print(inst.instId)
-    
+
     inst.write('*rst')
     inst.write('acquire:stopafter sequence')
     rl = inst.query('horizontal:mode:recordlength?')
@@ -135,15 +149,15 @@ def scope_example(ipAddress):
 
         inst.write('curve?')
         data = inst.binblockread(dtype=np.uint8, debug=True)
-    
+
     print(inst.get_error())
     inst.disconnect()
     return data
 
 
 def main():
-    data = scope_example('192.168.1.8')
+    scope_example('192.168.1.8')
 
-    
+
 if __name__ == '__main__':
     main()
